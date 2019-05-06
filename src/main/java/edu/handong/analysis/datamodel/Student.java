03fe568fa74129752bde319a6cfc92379eb242f3
MinYeSeul <21800251@handong.edu>
2019-05-06 14:49:39 +0900

    HGUCoursePatternAnalyzer

---
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -0,0 +1,14 @@
+package edu.handong.analysis.datamodel;
+
+public class Student {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public Student(String name) {
+		super();
+		this.name = name;
+	}
+}
