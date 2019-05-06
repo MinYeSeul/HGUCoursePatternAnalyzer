cff551e9c96801cb7cac75330bbea76172ffac07
MinYeSeul <21800251@handong.edu>
2019-05-06 14:45:05 +0900

    HGUCoursePatternAnalyzer

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -19,10 +19,10 @@ public class HGUCoursePatternAnalyzer {
 						"2019-1, SJ Kim, Algorithm Analysis",
 						};
 
-	int numOfStudents;
-	int numOfCourses;
-	Student[] students;
-	Course[] courses;
+	private int numOfStudents;
+	private int numOfCourses;
+	private Student[] students;
+	private Course[] courses;
 	
 	/**
 	 * This method runs our analysis logic to get the list of student and course names from lines.
@@ -33,6 +33,9 @@ public class HGUCoursePatternAnalyzer {
 		numOfStudents = Integer.parseInt(args[0]);
 		numOfCourses = Integer.parseInt(args[1]);
 	
+		/**
+		 * Initiate students
+		 */
 		students = initiateStudentArrayFromLines(lines);
 		
 		System.out.println("Number of All Students: " + numOfStudents);
@@ -56,13 +59,32 @@ public class HGUCoursePatternAnalyzer {
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
 		
 		// TODO: implement this method
+		/**
+		 * Initiate students of Student[] type
+		 */
+		students = new Student[numOfStudents];
+		int i = 0;
 		
-		
-		return null;
+		/**
+		 * Put divided lines to instanceOfStudent
+		 */
+		for(int k = 0; k < lines.length; k++) {
+			Student instanceOfStudent = new Student(lines[k].split(",")[1].trim());
+			
+			/**
+			 * If instanceOfStudent is not in students, input instanceOfStudent to students
+			 */
+			if(studentExist(students, instanceOfStudent) == false) {
+				students[i] = instanceOfStudent;
+				i++;
+			}
+			
+		}
+		return students;
 	}
 
 	/**
-	 * This method check if there is the same name of the second arugement in the array, students
+	 * This method check if there is the same name of the second argument in the array, students
 	 * @param students
 	 * @param student
 	 * @return boolean
@@ -70,6 +92,23 @@ public class HGUCoursePatternAnalyzer {
 	private boolean studentExist(Student[] students, Student student) {
 		
 		// TODO: implement this method
+		for(Student student_exist: students) {
+			
+			/**
+			 * If students is null, return false
+			 */
+			if (student_exist == null) {
+				return false;
+			}
+			else {
+				/**
+				 * If student and student_exist are same, return true
+				 */
+				if (student.getName().equals(student_exist.getName())) {
+					return true;
+				}
+			}
+		}
 
 		return false;
 	}
@@ -82,8 +121,28 @@ public class HGUCoursePatternAnalyzer {
 	private Course[] initiateCourseArrayFromLines(String[] lines) {
 		
 		// TODO: implement this method
+		/**
+		 * Initiate students of Course[] type
+		 */
+		courses = new Course[numOfCourses];
+		int i = 0;
 		
-		return null;
+		/**
+		 * Put divided lines to instanceOfCourse
+		 */
+		for(int k = 0; k < lines.length; k++) {	
+			Course instanceOfCourse = new Course(lines[k].split(",")[2].trim());
+			
+			/**
+			 * If instanceOfCourse is not in courses, input instanceOfCourse to courses
+			 */
+			if(courseExist(courses, instanceOfCourse) == false) {
+				courses[i] = instanceOfCourse;
+				i++;
+			}
+			
+		}
+		return courses;
 	}
 
 	/**
@@ -95,6 +154,22 @@ public class HGUCoursePatternAnalyzer {
 	private boolean courseExist(Course[] courses, Course course) {
 		
 		// TODO: implement this method
+		/**
+		 * If courses is null, return false
+		 */
+		for(Course course_exist: courses) {
+			if (course_exist == null) {
+				return false;
+			}
+			else {
+				/**
+				 * If course and course_exist are same, return true
+				 */
+				if (course.getCourseName().equals(course_exist.getCourseName())) {
+					return true;
+				}
+			}
+		}
 
 		return false;
 	}
